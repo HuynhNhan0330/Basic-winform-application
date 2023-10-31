3605df7f32dfcf2cb76ea17793b7430332d8ccad
huynhan03 <huynhnhandanang@gmail.com>
2023-11-01 03:24:33 +0700

    Add custom control

---
+++ MusicOnline/Utils/BaseColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicOnline.Utils
+{
+    public class BaseColor
+    {
+        public static Color XanhLa = Color.FromArgb(24, 155, 70);
+        public static Color Den = Color.FromArgb(0, 0, 0);
+        public static Color Trang = Color.FromArgb(255, 255, 255);
+        public static Color DenNhat = Color.FromArgb(36, 35, 34);
+        public static Color Xam = Color.FromArgb(77, 77, 77);
+    }
+}
