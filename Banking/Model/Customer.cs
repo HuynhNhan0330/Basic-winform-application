029363cd87c932ebbf240f48f74f57fe8842dd78
Huỳnh Mai Cao Nhân <huynhnhandanang@gmail.com>
2023-12-07 10:57:19 +0700

    update customer model

--- Banking/Model/Customer.cs
+++ Banking/Model/Customer.cs
@@ -10,6 +10,8 @@ namespace Banking.Model
     {
         public string CustomerID { get; set; }
         public string CustomerName { get; set; }
+        public string accountNumber { get; set; }
+        public string currentMoney { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime Birthday { get; set; }
         public string Email { get; set; }
